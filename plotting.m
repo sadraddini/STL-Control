dfb0d3428df5d991418755335539f2cefb00bcb4
Sadra Sadraddini <sadra@bu.edu>
2017-03-28 18:16:02 -0400

    End of STL-synthesis — will move to disturbances

--- plotting.m
+++ plotting.m
@@ -4,11 +4,15 @@ close all
 X=importdata('state.txt');
 [T,n]=size(X);
 hold on
-rectangle('Position',[3,5,2,2],'FaceColor',[1 0 0])
+set(gcf,'Units','inches',...
+ 'Position',[20 5 10 10])
+set(0,'defaultaxesposition',[0 0 1 1])
+rectangle('Position',[3,4,4,4],'FaceColor',[1 0 0])
 rectangle('Position',[1,7,1,1],'FaceColor',[0 1 0])
 rectangle('Position',[10,10,1,1],'FaceColor',[0 0 1])
 % axis([-5 15 -5 15])
 grid on
+axis('equal')
 for t=1:T
     plot(X(1:t,1),X(1:t,3),'--','LineWidth',1,'Color',[0 0 1])
     plot(X(1:t,1),X(1:t,3),'o','LineWidth',4,'Color',[1 0 0])
--- robot.py
+++ robot.py
@@ -3,8 +3,7 @@ from rci_family import *
 
 s=STL_system(4,2,16)
 
-obj=s.model.getObjective()
-print obj
+s.matrix_installation()
 
 s.A[1,1]=1
 s.A[1,2]=1
@@ -17,25 +16,15 @@ s.A[4,4]=1
 s.B[2,1]=1
 s.B[4,2]=1
 
-s.c[2,1]=0
-s.c[4,1]=0
-
 
 s.T=25
 
-s.A=complete_matrix(s.A)
-s.B=complete_matrix(s.B)
-s.c=complete_matrix(s.c)
-
-show_matrix(s.A)
-show_matrix(s.B)
-show_matrix(s.c)
 
 s.add_variables()        
 
-s.add_secondary_signal_state(1,[0,0,-1,0],5)    # -y+5>0 --> 5>y
-s.add_secondary_signal_state(2,[1,0,0,0],-5)    # x-5>0 --> x>5
-s.add_secondary_signal_state(3,[0,0,1,0],-7)    # y-7>0 --> y>7
+s.add_secondary_signal_state(1,[0,0,-1,0],4)    # -y+4>0 --> 4>y
+s.add_secondary_signal_state(2,[1,0,0,0],-7)    # x-7>0 --> x>7
+s.add_secondary_signal_state(3,[0,0,1,0],-8)    # y-8>0 --> y>8
 s.add_secondary_signal_state(4,[-1,0,0,0],3)    # -x+3>0 --> 3>x
 
 s.add_secondary_signal_state(5,[0,0,1,0],-10)   # y-10>0 --> y>10
@@ -48,10 +37,10 @@ s.add_secondary_signal_state(10,[-1,0,0,0],2)   # -x+2>0 --> 2>x
 s.add_secondary_signal_state(11,[0,0,1,0],-7)   # y-7>0 --> y>7
 s.add_secondary_signal_state(12,[0,0,-1,0],8)   # -y +8 > 0 --> 8>y
 
-s.add_secondary_signal_control(13,[1,0],20)    # u+100>0 --> u>-100
-s.add_secondary_signal_control(14,[-1,0],20)   # -u+100>0 --> 100>u
-s.add_secondary_signal_control(15,[0,1],20)
-s.add_secondary_signal_control(16,[0,-1],20)
+s.add_secondary_signal_control(13,[1,0],1)    # u+100>0 --> u>-100
+s.add_secondary_signal_control(14,[-1,0],1)   # -u+100>0 --> 100>u
+s.add_secondary_signal_control(15,[0,1],1)
+s.add_secondary_signal_control(16,[0,-1],1)
 
 s.add_formula("obstacle")
 s.disjunction("obstacle",[1,2,3,4])
@@ -74,7 +63,7 @@ s.always("phi_4","controls",range(0,s.T))
 s.add_formula("phi_whole")
 s.conjunction("phi_whole",["phi_1","phi_2","phi_3","phi_4"])
 
-s.initial_condition([1,0,4,0])
+s.initial_condition([0,0,0,0])
 s.integer_encoding()
 s.solve("phi_whole")
 s.write_to_file()
